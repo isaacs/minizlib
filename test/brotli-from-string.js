8c69ffef79c07f56e5ae901acc71bed708caf518
isaacs <i@izs.me>
2024-03-18 16:07:07 -0700

    port to ts, modernize everything

--- test/brotli-from-string.js
+++ test/brotli-from-string.js
@@ -1,41 +1,50 @@
-'use strict';
+'use strict'
 // Test compressing and uncompressing a string with brotli
 
-const t = require('tap')
-if (!require('zlib').BrotliDecompress) {
-  t.plan(0, 'brotli not supported')
-  process.exit(0)
-}
-const zlib = require('../');
+import t from 'tap'
+import {BrotliCompress, BrotliDecompress} from '../dist/esm/index.js'
 
-const inputString = 'ΩΩLorem ipsum dolor sit amet, consectetur adipiscing eli' +
-                    't. Morbi faucibus, purus at gravida dictum, libero arcu ' +
-                    'convallis lacus, in commodo libero metus eu nisi. Nullam' +
-                    ' commodo, neque nec porta placerat, nisi est fermentum a' +
-                    'ugue, vitae gravida tellus sapien sit amet tellus. Aenea' +
-                    'n non diam orci. Proin quis elit turpis. Suspendisse non' +
-                    ' diam ipsum. Suspendisse nec ullamcorper odio. Vestibulu' +
-                    'm arcu mi, sodales non suscipit id, ultrices ut massa. S' +
-                    'ed ac sem sit amet arcu malesuada fermentum. Nunc sed. ';
-const compressedString = 'G/gBQBwHdky2aHV5KK9Snf05//1pPdmNw/7232fnIm1IB' +
-                         'K1AA8RsN8OB8Nb7Lpgk3UWWUlzQXZyHQeBBbXMTQXC1j7' +
-                         'wg3LJs9LqOGHRH2bj/a2iCTLLx8hBOyTqgoVuD1e+Qqdn' +
-                         'f1rkUNyrWq6LtOhWgxP3QUwdhKGdZm3rJWaDDBV7+pDk1' +
-                         'MIkrmjp4ma2xVi5MsgJScA3tP1I7mXeby6MELozrwoBQD' +
-                         'mVTnEAicZNj4lkGqntJe2qSnGyeMmcFgraK94vCg/4iLu' +
-                         'Tw5RhKhnVY++dZ6niUBmRqIutsjf5TzwF5iAg8a9UkjF5' +
-                         '2eZ0tB2vo6v8SqVfNMkBmmhxr0NT9LkYF69aEjlYzj7IE' +
-                         'KmEUQf1HBogRYhFIt4ymRNEgHAIzOyNEsQM=';
+const inputString =
+  'ΩΩLorem ipsum dolor sit amet, consectetur adipiscing eli' +
+  't. Morbi faucibus, purus at gravida dictum, libero arcu ' +
+  'convallis lacus, in commodo libero metus eu nisi. Nullam' +
+  ' commodo, neque nec porta placerat, nisi est fermentum a' +
+  'ugue, vitae gravida tellus sapien sit amet tellus. Aenea' +
+  'n non diam orci. Proin quis elit turpis. Suspendisse non' +
+  ' diam ipsum. Suspendisse nec ullamcorper odio. Vestibulu' +
+  'm arcu mi, sodales non suscipit id, ultrices ut massa. S' +
+  'ed ac sem sit amet arcu malesuada fermentum. Nunc sed. '
+const compressedString =
+  'G/gBQBwHdky2aHV5KK9Snf05//1pPdmNw/7232fnIm1IB' +
+  'K1AA8RsN8OB8Nb7Lpgk3UWWUlzQXZyHQeBBbXMTQXC1j7' +
+  'wg3LJs9LqOGHRH2bj/a2iCTLLx8hBOyTqgoVuD1e+Qqdn' +
+  'f1rkUNyrWq6LtOhWgxP3QUwdhKGdZm3rJWaDDBV7+pDk1' +
+  'MIkrmjp4ma2xVi5MsgJScA3tP1I7mXeby6MELozrwoBQD' +
+  'mVTnEAicZNj4lkGqntJe2qSnGyeMmcFgraK94vCg/4iLu' +
+  'Tw5RhKhnVY++dZ6niUBmRqIutsjf5TzwF5iAg8a9UkjF5' +
+  '2eZ0tB2vo6v8SqVfNMkBmmhxr0NT9LkYF69aEjlYzj7IE' +
+  'KmEUQf1HBogRYhFIt4ymRNEgHAIzOyNEsQM='
 
-t.test('compress then decompress', t =>
-  new zlib.BrotliCompress().end(inputString).concat().then(buffer => {
-    t.ok(inputString.length > buffer.length, 'buffer is shorter than input')
+t.test('compress then decompress', async t =>
+  new BrotliCompress()
+    .end(inputString)
+    .concat()
+    .then(async buffer => {
+      t.ok(
+        inputString.length > buffer.length,
+        'buffer is shorter than input',
+      )
 
-    return new zlib.BrotliDecompress().end(buffer).concat().then(buffer =>
-      t.equal(buffer.toString(), inputString))
-  }))
+      return new BrotliDecompress()
+        .end(buffer)
+        .concat()
+        .then(buffer => t.equal(buffer.toString(), inputString))
+    }),
+)
 
 t.test('decompress then check', t =>
-  new zlib.BrotliDecompress({ encoding: 'utf8' })
-    .end(compressedString, 'base64').concat().then(result =>
-      t.equal(result, inputString)))
+  new BrotliDecompress({ encoding: 'utf8' })
+    .end(compressedString, 'base64')
+    .concat()
+    .then(result => t.equal(result, inputString)),
+)
--- test/dictionary-fail.js
+++ test/dictionary-fail.js
@@ -1,38 +1,39 @@
-'use strict'
-const t = require('tap')
-const zlib = require('../')
+import t from 'tap'
+import { Inflate, InflateRaw } from '../dist/esm/index.js'
 
 // String "test" encoded with dictionary "dict".
-const input = Buffer.from([0x78, 0xBB, 0x04, 0x09, 0x01, 0xA5])
+const input = Buffer.from([0x78, 0xbb, 0x04, 0x09, 0x01, 0xa5])
 
 {
-  const stream = new zlib.Inflate()
+  const stream = new Inflate()
 
   stream.on('error', err =>
     t.match(err, {
       message: 'zlib: Missing dictionary',
       errno: 2,
       code: 'Z_NEED_DICT',
-    }))
+    }),
+  )
 
   stream.write(input)
 }
 
 {
-  const stream = new zlib.Inflate({ dictionary: Buffer.from('fail') })
+  const stream = new Inflate({ dictionary: Buffer.from('fail') })
 
   stream.on('error', err =>
     t.match(err, {
       message: 'zlib: Bad dictionary',
       errno: 2,
       code: 'Z_NEED_DICT',
-    }))
+    }),
+  )
 
   stream.write(input)
 }
 
 {
-  const stream = new zlib.InflateRaw({ dictionary: Buffer.from('fail') })
+  const stream = new InflateRaw({ dictionary: Buffer.from('fail') })
 
   // It's not possible to separate invalid dict and invalid data when
   // using the raw format
@@ -41,7 +42,8 @@ const input = Buffer.from([0x78, 0xBB, 0x04, 0x09, 0x01, 0xA5])
       message: 'zlib: invalid stored block lengths',
       errno: -3,
       code: 'Z_DATA_ERROR',
-    }))
+    }),
+  )
 
   stream.write(input)
 }
